0cfcc6e8e458d76b768f5ab33441562ef0be744b
pjlao307 <pjlao307@gmail.com>
2019-06-05 08:25:43 -0700

    Add script to handle reverting bad bootup

---
+++ checkLastBoot.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import os
+import subprocess
+
+apk_loaded = "/data/params/d/APKLoaded"
+last_boot = "/data/communitypilot_scripts/lastboot"
+file_exists = os.path.exists("%s" % apk_loaded)
+
+loaded = False
+
+if file_exists:
+  f = open("%s" % apk_loaded, "r")
+  loaded = True if f.read().rstrip('\r\n') == '1' else False
+  f.close()
+else:
+  username = subprocess.check_output("ls -ld /data/openpilot | perl -lne 'print $1 if /-\> (.*)/'", shell=True)
+  os.system("echo %s > /data/communitypilot_scripts/lastboot" % username.rstrip('\r\n'))
+  os.system("echo 1 > %s" % apk_loaded)
+  loaded = True
+
+if (not loaded):
+  # Revert to last good booted repo
+  f = open("%s" % last_boot, "r")
+  lastboot = f.read().rstrip('\r\n')
+  cmd = "ln -s /data/%s /data/openpilot" % lastboot
+  os.system("rm /data/openpilot")
+  os.system("%s" % cmd)
+else:
+  username = subprocess.check_output("ls -ld /data/openpilot | perl -lne 'print $1 if /-\> (.*)/'", shell=True)
+  os.system("echo %s > /data/communitypilot_scripts/lastboot" % username.rstrip('\r\n'))
+
