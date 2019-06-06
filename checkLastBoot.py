9711ae51c0e2e553a5c2eb19c1db61f7930789e7
pjlao307 <pjlao307@gmail.com>
2019-06-05 23:03:56 -0700

    Update checkLastBoot.py
    Fix syntax error

--- checkLastBoot.py
+++ checkLastBoot.py
@@ -14,7 +14,7 @@ if file_exists:
   f.close()
 else:
   username = subprocess.check_output("ls -ld /data/openpilot | perl -lne 'print $1 if /-\> (.*)/'", shell=True)
-  os.system("echo %s > %s" % (username.rstrip('\r\n'), last_boot)
+  os.system("echo %s > %s" % (username.rstrip('\r\n'), last_boot))
   os.system("echo 1 > %s" % apk_loaded)
   loaded = True
 
